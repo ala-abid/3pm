f3fa9c6618729b3c918506f3777b96ba897ccb34
ala-abid <abidala99@yahoo.fr>
2018-07-26 13:15:39 +0100

    minor changes

--- src/app/services/interceptor.service.ts
+++ src/app/services/interceptor.service.ts
@@ -23,6 +23,8 @@ export class InterceptorService implements HttpInterceptor {
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error.message);
+    } else if ( error.status === 401){
+      console.log('nope');
     } else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong,
