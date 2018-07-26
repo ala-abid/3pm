ee89629b0babf8368b0a7089b863ad0ebce65083
ala-abid <abidala99@yahoo.fr>
2018-07-26 13:17:04 +0100

    minor changes

--- src/app/AppConfig.ts
+++ src/app/AppConfig.ts
@@ -2,5 +2,6 @@
 export const  baseUrl = 'http://www.mocky.io/v2' ;
 
 
-export const loginUrl = '/5b599b9d2f00000b175f9481' ;
+export const loginUrl = '/5b583a28300000fd05fe4df9' ;
 export const userUrl = '/5b583a7b3000004900fe4dfb' ;
+export const error401Url = '/5b599b9d2f00000b175f9481';
